9b46ec1a291ca4fb16c7ae1feac8eb28dd381c67
Tanishq sharma <66619762+tanishq201@users.noreply.github.com>
2020-08-10 18:55:48 +0530

    Add files via upload

---
+++ Raddissonblue.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,800&display=swap" rel="stylesheet">
+    <style>h1{
+  font-family: 'Dancing Script', cursive;
+}</style>
+<style>.design{
+  font-family: 'Montserrat', sans-serif;
+}</style>
+<style>.designing{
+  font-family: 'Montserrat', sans-serif;
+  margin-top: 60px;
+}</style>
+<style>#perfection{
+  
+  background-color: black;
+  color: white;
+  padding-top: 5px;
+    margin-right: 200px;
+    text-align: center;
+    padding-bottom: 10px;
+}</style>
+<style>.perfect{
+  
+  background-color: black;
+  color: white;
+  padding-top: 5px;
+    margin-right: 400px;
+    text-align: center;
+    padding-bottom: 10px;
+    margin-top: 40px;
+}</style>
+    <title>Raddison blue</title>
+  </head>
+  <body class="container" style="background-color: grey;">
+    <h1>Raddisson Blu</h1>
+    <h3 class="perfect">Relax in spacious accommodations with free Wi-Fi</h3>
+<p class="design">Our contemporary hotel features 175 large rooms and expansive suites for business or leisure travelers, complete with free Wi-Fi and in-room coffee and tea facilities. Business Class guests are welcome to perks like free breakfast and complimentary drinks in the evening. Upgrade to a suite to enjoy a separate living room and personal touches like turndown service.</p>
+<div class="row">
+  <div class="col-sm-4">
+<img src="https://media.radissonhotels.net/image/radisson-blu-hotel-guwahati/exteriorview/16256-114051-f63650959_3xl.jpg?impolicy=HomeHero" style="height: 200px;width: 340px;" class="img-fluid" alt="Responsive image">
+</div>
+<div class="col-sm-4">
+<img src="https://content3.jdmagicbox.com/comp/ghaziabad/v9/011pxx11.xx11.140905105539.m3v9/catalogue/radisson-blu-hotel-kaushambi-ghaziabad-hotels-rs-5001-to-rs-6000--254ua6c.jpg?clr=002a66" style="height: 200px;width: 340px;" class="img-fluid" alt="Responsive image">
+</div>
+<div class="col-sm-4">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSTSeaAGsL79G1f8fFHULpUDZ9u9W23Ldu75g&usqp=CAU" style="height: 200px;width: 340px;" class="img-fluid" alt="Responsive image">
+</div>
+</div>
+<h3 style="margin-top: 20px;" id="perfection">Host a conference or wedding reception in our flexible meeting space</h3>
+<div class="row" style="margin-bottom: 40px;">
+  <div class="col-sm-6">
+<p class="designing">Located in the heart of the industrial and IT hub, just a few kilometers from the India Expo, our hotel meeting space is the prime location to host a conference or event. The Royal Ballroom & Victoria Hall, spanning 1,115 square meters (12,000 square feet), makes for an excellent venue for events of any type. We offer two boardrooms in our business center and dedicated staff to assist with planning.</p>
+</div>
+<div class="col-sm-6">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSjEUwGdb2ujK_GFVfrg1k5PN_2hHfGMCttxQ&usqp=CAU" class="img-fluid" alt="Responsive image">
+</div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ tajhotel.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Courgette&display=swap" rel="stylesheet">
+    <style>h1{
+  font-family: 'Alfa Slab One', cursive;
+  text-decoration: underline;
+}</style>
+<style>.dist{
+  background-color: black;
+  color: white;
+  margin-right: 970px;
+  padding: 5px;
+  margin-top: 30px;
+}</style>
+<style>.style{
+  font-family: 'Alfa Slab One', cursive;
+font-family: 'Courgette', cursive;
+}</style>
+<style>.shrinking{
+  height: 300px;
+  border-radius: 50%;
+}</style>
+<style>.disting{
+  background-color: black;
+  color: white;
+  margin-top: 20px;
+  margin-right: 400px;
+  padding: 5px;
+}</style>
+    <title>Taj hotel</title>
+  </head>
+  <body class="container" style="background-color: grey;">
+    <h1>Taj Hotel</h1>
+    <h3 class="dist">About Taj</h3>
+<p class="style">Taj, India’s Strongest Brand across industries and sectors*, is an iconic hospitality brand from the Indian Hotels Company Limited. The brand continues to be one of the most revered and loved hospitality brands with a legacy of over 116 years of impeccable service and genuine warmth. Embodying the spirit of “Tajness”, authentic original palaces, landmark hotels, idyllic resorts and natural safari lodges interpret the tradition of hospitality in a refreshingly modern way to create unique experiences and lifelong memories.</p>
+<div class="row">
+  <div class="col-sm-4">
+    <img src="https://d2c7ipcroan06u.cloudfront.net/wp-content/uploads/2019/08/Taj-hotel-Mumbai-colaba.jpeg" class="shrinking" class="img-fluid" alt="Responsive image" style="height: 250px; width: 250px;">
+  </div>
+  <div class="col-sm-4">
+    <img src="https://i0.wp.com/metrosaga.com/wp-content/uploads/2020/06/Taj-Hotel-Mumbai-1.jpg?fit=1200%2C628&ssl=1" class="shrinking" class="img-fluid" alt="Responsive image" style="height: 250px; width: 250px;">
+  </div>
+  <div class="col-sm-4">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRcPxDGd5KVzCqtypD-510Sn_6YXuoXg2uJpw&usqp=CAU" class="shrinking" class="img-fluid" alt="Responsive image" style="height: 250px; width: 250px;">
+  </div>
+</div>
+<div class="row" style="margin-bottom: 30px;">
+  <div class="col-sm-6">
+    <h3 class="disting">Taj Hotels</h3>
+<p class="style">Forming a major part of IHCL, Taj Hotels are renowned worldwide for their extraordinary commitment towards providing luxury hospitality. Each of the luxury hotels sets an example of opulence—born from flavourful local culture and world-class amenities. At Taj, guests from around the world find comfort in the stately, tastefully designed rooms, offering unhindered views of the destination, while providing convenient in-room facilities.
+
+Another discerning aspect of Taj is their ability to provide a culinary experience unlike any other. Many of our award-winning restaurants serve both, as well as the essentially global cuisine; providing guests with a masterfully balanced taste.
+
+Fulfilling the purpose of a guest’s visit takes top priority at Taj. Be it a significant meeting, a crucial conference, a much-awaited get-together, or an affair of a lifetime—a wedding; at Taj, occasions turn into memories. Plush banquets adapt seamlessly to the nature of the event, while functioning with incredible efficiency and poise, displaying Taj’s spirit of hospitality at its peak.</p>
+</div>
+<div class="col-sm-6">
+<img src="https://luxurytravelspots.com/wp-content/uploads/2012/12/Taj-Hotels-in-India-Bathroom.jpg" style="height: 400px; width: 500px; margin-top: 60px; mar">
+  </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
